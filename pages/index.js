1f9dc99ed5505a62b1a90fa6c6073918c51df77b
Dan Uhl <duhl@wayfair.com>
2022-03-15 09:34:09 -0400

    adds all the files

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const IMAGE_SOURCE = 'https://dog.ceo/api/breeds/image/random';
+
+export async function getStaticProps() {
+  const picResults = await Promise.all(
+    Array.from(new Array(10), (_) =>
+      fetch(IMAGE_SOURCE).then((res) => res.json()),
+    ),
+  );
+  const pics = picResults.map((r) => r.message);
+  return { props: { pics } };
+}
+
+function HomePage({ pics }) {
+  return (
+    <div>
+      <h1>Doggos!</h1>
+      {pics.map((i) => (
+        <img src={i} />
+      ))}
+    </div>
+  );
+}
+
+export default HomePage;
