178ffba88d09bf7586a6f075a998248d64adfaf6
Dan Uhl <dan.uhl@posh.tech>
2024-08-30 08:51:59 -0400

    first

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const IMAGE_SOURCE = 'https://dog.ceo/api/breeds/image/random';
+const IMAGE_SOURCE = "https://dog.ceo/api/breeds/image/random";
 
 export async function getStaticProps() {
   const picResults = await Promise.all(
     Array.from(new Array(10), (_) =>
-      fetch(IMAGE_SOURCE).then((res) => res.json()),
-    ),
+      fetch(IMAGE_SOURCE).then((res) => res.json())
+    )
   );
   const pics = picResults.map((r) => r.message);
   return { props: { pics } };
